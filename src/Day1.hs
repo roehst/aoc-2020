d2e0cf1f760fea0e50d0983ec54e0a4c9ed50d3a
Rodrigo Stevaux <rodrigo.stevaux@loft.com.br>
2020-12-01 14:14:23 -0300

    Add comments to Day 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -2,18 +2,27 @@ module Day1 where
 
 import Control.Monad
 
+-- Day 1 was easy. We are given a list
+-- of numbers and we must find a pair of numbers that
+-- adds to 2020, and then a triple of numbers that adds
+-- to 2020.
+
+-- The solution was easy using the list monad.
 
 day1 :: IO ()
 day1 = do
 
     numbers <- map read <$> lines <$> readFile "inputs/Day1.txt"
 
+    -- We could pick all solutions, but
+    -- we only need one for the puzzle.
     let (a, b) = head $ solve2 numbers
 
     print a
     print b
     print (a*b)
 
+    -- Same story but with 3 numbers.
     let (a, b, c) = head $ solve3 numbers
 
     print a
@@ -22,12 +31,18 @@ day1 = do
     print (a*b*c)
 
 
+-- Here I use the list monad to filter through all pairs of
+-- numbers and return only those that satisfy some
+-- criteria.
+-- This is equivalent to using list comprehensions.
+solve2 :: [Int] -> [(Int, Int)
 solve2 numbers = do
     a <- numbers
     b <- numbers 
     guard $ a + b == 2020
     return (a, b)
 
+solver3 :: [Int] -> [(Int, Int, Int)]
 solve3 numbers = do
     a <- numbers
     b <- numbers 
